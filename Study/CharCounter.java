471514414658763104f3f1482baad1aeebb5a17c
SG-ShivaniGupta <sgshivanigupta60@gmail.com>
2015-05-21 17:33:05 +0530

    Update CharCounter.java

--- Study/CharCounter.java
+++ Study/CharCounter.java
@@ -3,17 +3,17 @@ public class CharCounter {
     public static void main(String[] args) {
 		
 		System.out.print("Enter String = ");
-		Scanner text = new Scanner(System.in);
-		String text1 = text.next();
+		Scanner scanner1 = new Scanner(System.in);
+		String text = scanner1.next();
 		
 		System.out.print("Enter Letter = ");
-		Scanner char1 = new Scanner(System.in);
-		char char2 = char1.next().charAt(0);
+		Scanner scanner2 = new Scanner(System.in);
+		char char1 = scanner2.next().charAt(0);
 		//String text1 = "madam";
 		//char char2 = 'a';
 
-        int numberOfLetter = CharCounter.countCharOccurrences(text1, char2);
-        System.out.println("Letter '" + char2 + "' occurs = '" + numberOfLetter + "' times.");
+        int numberOfLetter = CharCounter.countCharOccurrences(text, char1);
+        System.out.println("Letter '" + char1 + "' occurs = '" + numberOfLetter + "' times.");
     }
 
     
